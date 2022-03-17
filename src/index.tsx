8a255f93d9242237057f421d355d30b63df3b537
Toan Phan <toanphan1302@gmail.com>
2022-03-17 11:32:27 +0700

    unpkg_plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import  ReactDOM  from "react-dom";
 import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
 
@@ -13,12 +14,16 @@ const App = () => {
             return;
         }
 
-        const result = await ref.current.transform(input, {
-            loader: 'jsx',
-            target: 'es2015'
+        const result = await ref.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()]
         });
 
-        setCode(result.code);
+        // console.log(result);
+
+        setCode(result.outputFiles[0].text);
     };
 
     const startService = async () => {
